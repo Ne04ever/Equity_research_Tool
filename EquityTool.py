24e37ff199b8c468963972b839fc4dbe726de5d7
Ne04ever <72649171+Ne04ever@users.noreply.github.com>
2024-09-10 13:10:25 +0530

    Update EquityTool.py

--- EquityTool.py
+++ EquityTool.py
@@ -2,7 +2,6 @@ import os
 import streamlit as st
 import pickle
 import time
-from langchain import OpenAI
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import UnstructuredURLLoader
